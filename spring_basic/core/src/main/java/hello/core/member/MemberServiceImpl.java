e544338ae16fd76037c08a3f233d73ae2a3b9eea
seo-jio <lio8625@naver.com>
2023-03-14 22:59:19 +0900

    순수 자바로 코딩

---
+++ spring_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,14 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //추상화와 구체화 모두 의존
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ spring_basic/core/src/test/java/hello/core/member/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L; //wrapper class를 쓰는 이유는 null이 들어갈 수도 있기 때문이다.
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        // when
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
