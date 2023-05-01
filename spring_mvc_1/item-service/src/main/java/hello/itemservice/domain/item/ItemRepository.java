3d8bc2f236e93ead457786c8195c5b0e731481eb
seo-jio <lio8625@naver.com>
2023-05-01 22:50:24 +0900

    스프링 mvc - 웹페이지 만들기 1

---
+++ spring_mvc_1/item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    // 멀티 쓰레드 환경에서는 ConcurrentHashMap, AtomicLong을 사용해야한다.
+    private static final Map<Long, Item> store = new HashMap<>(); // static
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    // 실제 개발할 때는 Item이 아니라 ItemDto를 만들어서 사용해야 한다.
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ spring_mvc_1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,42 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final이 붙은 멤버로 생성자를 자동으로 만들어 준다.
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
